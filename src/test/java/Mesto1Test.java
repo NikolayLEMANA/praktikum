e790c1ae584c9ded53d34fdc0e8c6e4ce9ba88fc
Nikolay <nikolay.voronov@leroymerlin.ru>
2025-04-24 22:23:56 +0300

    2

--- src/test/java/Mesto1Test.java
+++ src/test/java/Mesto1Test.java
@@ -10,7 +10,7 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class Mesto1Test {
 
-    String bearerToken = "сюда_впиши_свой_токен";
+    String bearerToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2MzMDg4ZDRkY2Q5MzAwM2QwZDMwZGIiLCJpYXQiOjE3NDU1MjI1NDYsImV4cCI6MTc0NjEyNzM0Nn0.EayThbDy5x70WWRsykGW37At4_0txkcrbUcPQPYrnfg";
 
     @Before
     public void setUp() {
